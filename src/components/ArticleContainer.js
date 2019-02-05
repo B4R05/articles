5093ac4992968c1176716907f64a58b249e10900
Gav Barosee <gav@Gavs-MacBook-Pro.local>
2019-02-05 14:06:22 +0000

    Fetch a random article
    ArticleContainer can now fetch a random article upon first mounting.
    
    A random integer between 1 and 5 is generated and passed to the api get request. Each number sent to the backend requests a JSON article object associated with that number.
    
    For the time being, we can fetch the same article twice or more but this will be fixed later on where we will fetch only one article at a time and not have it fetched again

--- src/components/ArticleContainer.js
+++ src/components/ArticleContainer.js
@@ -3,6 +3,19 @@ import Article from "./Article";
 import api from "../api/api";
 
 class ArticleContainer extends Component {
+  componentDidMount() {
+    this.fetchArticle();
+  }
+
+  fetchArticle = () => {
+    let randomNumber = Math.floor(Math.random() * 5) + 1;
+
+    api
+      .get(`/${randomNumber}`)
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  };
+
   render() {
     return (
       <div>
